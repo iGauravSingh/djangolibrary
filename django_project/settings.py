eeb036585002edea85e719c1f113b45ed6366702
iGauravSingh <poetonroad@gmail.com>
2024-05-22 09:43:04 +0530

    api made

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    "rest_framework", #new
     # Local
     "books.apps.BooksConfig", # New
+    "apis.apps.ApisConfig", # New
 ]
 
 MIDDLEWARE = [
